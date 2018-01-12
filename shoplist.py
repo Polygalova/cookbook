07194f80c212638d9dd5333f98c46eac13192e77
polygalova <polygalova@yandex-team.ru>
2018-01-12 10:20:16 +0300

    set, dict instead of list

--- shoplist.py
+++ shoplist.py
@@ -1,34 +1,28 @@
-def is_in(shop_list, new_ing):
-    for i in range(len(shop_list)):
-        if new_ing['ingridient_name'] == shop_list[i]['ingridient_name']:
-            return i
-
-
 def get_shop_list():
     n = int(input('Сколько человек?'))
-    dishes = input('Что будем готовить?').lower().strip().split(', ')
-    shop_list = []
+    dishes = set(input('Что будем готовить?').lower().strip().split(', '))
+    shop_list = {}
+    ing_meas = {}
     with open('cookbook.txt', encoding='utf8') as f:
         for line in f:
             if line.lower().strip() in dishes:
                 quan_ing = int(f.readline().strip())
                 for i in range(quan_ing):
                     ing, quan, meas = f.readline().strip().split(' | ')
-                    new_ing = {'ingridient_name': ing, 'quantity': int(quan), 'measure': meas}
-                    result = is_in(shop_list, new_ing)
-                    if result is None:
-                        shop_list.append(new_ing)
+                    if ing not in shop_list.keys():
+                        shop_list[ing] = int(quan)
+                        ing_meas[ing] = meas
                     else:
-                        shop_list[result]['quantity'] += new_ing['quantity']
-    for ingredient in shop_list:
-        ingredient['quantity'] *= n
-    return shop_list
+                        shop_list[ing] += int(quan)
+    for quan in shop_list.values():
+        quan *= n
+    return shop_list, ing_meas
 
 
 def print_shop_list():
-    shop_list = get_shop_list()
-    for shop_list_item in shop_list:
-        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+    shop_list, ing_meas = get_shop_list()
+    for ing, quan in shop_list.items():
+        print('{} {} {}'.format(ing, quan, ing_meas[ing]))
 
 
 print_shop_list()
